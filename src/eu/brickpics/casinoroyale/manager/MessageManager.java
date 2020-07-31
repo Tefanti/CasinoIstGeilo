676a99393a9afaf1afa509f99ecbc7e0d50e619a
KeinOptifine <madrian.gurke@gmail.com>
2020-07-31 12:24:43 +0200

    Prefix, MessageManager

--- src/eu/brickpics/casinoroyale/manager/MessageManager.java
+++ src/eu/brickpics/casinoroyale/manager/MessageManager.java
@@ -1,17 +1,35 @@
 package eu.brickpics.casinoroyale.manager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
+
 
 public class MessageManager {
 
-    public static String getString(Player p, String id) {
-        return "";
+    ResourceBundle meldunge_de = ResourceBundle.getBundle("messages", Locale.GERMAN);
+    ResourceBundle meldunge_en = ResourceBundle.getBundle("messages", Locale.ENGLISH);
+
+    public static Locale intToLocale(int i) {
+        Locale result = null;
+
+        switch (i) {
+            case 0:
+                result = new Locale("en");
+                break;
+            case 1:
+                result = new Locale("de");
+                break;
+            default:
+                Bukkit.getLogger().warning("public static Locale intToLocale(int i) wurde mit i = " + i + " aufgerufen. i existiert nicht");
+        }
+
+        return result;
     }
 
-    public static Locale getLocale(Player p) {
-        return new Locale("en");
-    }
+
+
 
 }
--- src/eu/brickpics/casinoroyale/storage/Data.java
+++ src/eu/brickpics/casinoroyale/storage/Data.java
@@ -3,5 +3,5 @@ package eu.brickpics.casinoroyale.storage;
 import org.bukkit.ChatColor;
 
 public class Data {
-    public static final String PREFIX = ChatColor.DARK_PURPLE + "•" + ChatColor.LIGHT_PURPLE + "● Casino " + ChatColor.WHITE;
+    public static final String PREFIX = ChatColor.DARK_PURPLE + "•" + ChatColor.LIGHT_PURPLE + "● Casino §7";
 }
